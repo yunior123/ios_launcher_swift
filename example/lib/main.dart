a99e5586ae1befe2d8ede31cce94425e75207e7d
Yunior Rodriguez <47259501+yunior123@users.noreply.github.com>
2022-03-28 09:32:08 -0400

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,34 +8,8 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    try {
-      await IosLauncherSwift.launchUrl(
-          "https://docs.swift.org/swift-book/LanguageGuide/TypeCasting.html");
-    } on PlatformException {
-      print('Failed to get platform version.');
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,8 +18,18 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: const Center(
-          child: Text('Launching url'),
+        body: Center(
+          child: TextButton(
+            child: const Text("Launch Url"),
+            onPressed: () async {
+              try {
+                await IosLauncherSwift.launchUrl(
+                    "https://docs.swift.org/swift-book/LanguageGuide/TypeCasting.html");
+              } on PlatformException {
+                print('Failed to launch url.');
+              }
+            },
+          ),
         ),
       ),
     );
